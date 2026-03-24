1fa0a26b59aee829c3d3281254dfc573a8c5a5bf
sibom007 <vaispark400@gmail.com>
2026-03-24 12:09:07 +0600

    fix 1

--- src/app/module/Auth/auth.contorler.ts
+++ src/app/module/Auth/auth.contorler.ts
@@ -9,11 +9,18 @@ const LoginUser = catchAsync(async (req, res) => {
 
   res.cookie("refreshToken", result.refreshToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     path: "/",
   });
+  // res.cookie("refreshToken", result.refreshToken, {
+  //   httpOnly: true,
+  //   secure: process.env.NODE_ENV === "production",
+  //   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  //   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  //   path: "/",
+  // });
 
   sendResponse(res, {
     statusCode: 200,
